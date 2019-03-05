7713a92f0958b21bc75585e23802599766729d90
Gitanjali Mule <gitanjalidmule@gmail.com>
2019-03-05 21:39:56 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,7 +1,10 @@
 # your code goes here
 def begins_with_r(array)
-  if x.start_with?"r"
-    return true
-  else false
+  array.each do |x|
+    if x.start_with?"r"
+      return true
+    else false
+    end
   end
+  
 end
